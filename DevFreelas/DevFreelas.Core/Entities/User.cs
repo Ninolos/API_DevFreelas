579bd4acc854a53bb6861001f002e0fb6dc3abfa
Jonathan Daniel Loth <ninoloth@gmail.com>
2022-05-23 22:51:46 -0300

    Created base layers
    Entities, Enums and Repository for User and project

---
+++ DevFreelas/DevFreelas.Core/Entities/User.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreelas.Core.Entities
+{
+    public class User : BaseEntity
+    {
+        public User(string fullName, string email, DateTime birthDate)
+        {
+            FullName = fullName;
+            Email = email;
+            BirthDate = birthDate;
+            Active = true;
+
+            CreatedAt = DateTime.Now;
+            Skills = new List<UserSkill>();
+            OwnedProjects = new List<Projects>();
+            FreelanceProjects = new List<Projects>();
+
+        }
+
+        public string FullName { get; private set; }
+
+        public string Email { get; private set; }
+
+        public DateTime BirthDate { get; private set; }
+
+        public DateTime CreatedAt { get; private set; }
+
+        public bool Active { get; set; }
+
+        public List<UserSkill> Skills { get; private set; }
+
+        public List<Projects> OwnedProjects { get; private set; }
+
+        public List<Projects> FreelanceProjects { get; private set; }
+    }
+}
