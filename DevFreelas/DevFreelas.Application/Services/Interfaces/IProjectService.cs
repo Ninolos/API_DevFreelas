37fa28f2ea0bbd0ff582f86dfa8ef9cf27ba7684
Jonathan Daniel Loth <ninoloth@gmail.com>
2022-05-24 10:02:50 -0300

    Init Application layer

---
+++ DevFreelas/DevFreelas.Application/Services/Interfaces/IProjectService.cs
@@ -0,0 +1,30 @@
+﻿using DevFreelas.Application.InputModels;
+using DevFreelas.Application.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreelas.Application.Services.Interfaces
+{
+    public interface IProjectService
+    {
+        //methods from ProjectsController
+        List<ProjectViewModel> GetAll(string query);
+
+        ProjectDetailsViewModel GetById(string query);
+
+        int Create(NewProjectInputModel inputModel);
+
+        void Update(UpdateProjectInputModel inputModel);
+
+        void Delete(int id);
+
+        void CreateComment(CreateCommentInputModel inputModel);
+
+        void Start(int id);
+
+        void Finish(int id);
+    }
+}
