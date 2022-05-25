67b662c7525c880786ff03b2b9a87c430eb50c8c
Jonathan Daniel Loth <ninoloth@gmail.com>
2022-05-25 08:42:22 -0300

    Init Entity Framework Core Configs

--- DevFreelas/DevFreelas.Infrastructure/Persistence/DevFreelasDbContext.cs
+++ DevFreelas/DevFreelas.Infrastructure/Persistence/DevFreelasDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using DevFreelas.Core.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,40 +8,24 @@ using System.Threading.Tasks;
 
 namespace DevFreelas.Infrastructure.Persistence
 {
-  
-    public class DevFreelasDbContext
+
+    public class DevFreelasDbContext : DbContext
     {
-        public DevFreelasDbContext()
+        public DevFreelasDbContext(DbContextOptions<DevFreelasDbContext> options) : base(options)
         {
-            Projects = new List<Projects>
-            {
-                new Projects("My ASPNET Core Project 1", "My Description", 1, 1, 10000),
-                new Projects("My ASPNET Core Project 2", "My Description", 1, 1, 20000),
-                new Projects("My ASPNET Core Project 3", "My Description", 1, 1, 30000)
-            };
 
-            Users = new List<User>
-            {
-                new User("Jonathan Loth", "ninoloth@gmail.com", new DateTime(1995, 07, 20)),
-                new User("Robert C Martin", "test1@gmail.com", new DateTime(1995, 01, 15)),
-                new User("Anderson Sauberlich", "test2@gmail.com", new DateTime(1992, 07, 20))
-            };
-
-            Skills = new List<Skill>
-            {
-                new Skill(".NET Core"),
-                new Skill("C#"),
-                new Skill("SQL Server")
-            };
+        }
 
 
+        public DbSet<Projects> Projects { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Skill> Skills { get; set; }
+
+        public DbSet<UserSkill> UserSkills { get; set; }
+
+        public DbSet<ProjectComment> ProjectComments { get; set; }
     }
-        public List<Projects> Projects { get; set; }
 
-        public List<User> Users { get; set; }
-
-        public List<Skill> Skills { get; set; }
-
-        public List<ProjectComment> ProjectComments { get; set; }
-    }
 }
