acf9ec25cc48cc167dcbb456dacb4eb01a93f0ac
Jonathan Daniel Loth <ninoloth@gmail.com>
2022-05-25 13:51:29 -0300

    refactoring configurations

---
+++ DevFreelas/DevFreelas.Infrastructure/Persistence/Configurations/ProjectConfigurations.cs
@@ -0,0 +1,33 @@
+﻿using DevFreelas.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreelas.Infrastructure.Persistence.Configurations
+{
+    public class ProjectConfigurations : IEntityTypeConfiguration<Projects>
+    {
+        public void Configure(EntityTypeBuilder<Projects> builder)
+        {
+
+            builder
+                .HasKey(p => p.Id);
+
+            builder
+                .HasOne(p => p.Freelancer)
+                .WithMany(f => f.FreelanceProjects)
+                .HasForeignKey(p => p.IdFreelancer)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder
+                .HasOne(p => p.Client)
+                .WithMany(f => f.OwnedProjects)
+                .HasForeignKey(p => p.IdClient)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
--- DevFreelas/DevFreelas.Infrastructure/Persistence/DevFreelasDbContext.cs
+++ DevFreelas/DevFreelas.Infrastructure/Persistence/DevFreelasDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -29,48 +30,9 @@ namespace DevFreelas.Infrastructure.Persistence
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Projects>()
-                .HasKey(p => p.Id);
-
-            modelBuilder.Entity<Projects>()
-                .HasOne(p => p.Freelancer)
-                .WithMany(f => f.FreelanceProjects)
-                .HasForeignKey(p => p.IdFreelancer)
-                .OnDelete(DeleteBehavior.Restrict);
-
-            modelBuilder.Entity<Projects>()
-                .HasOne(p => p.Client)
-                .WithMany(f => f.OwnedProjects)
-                .HasForeignKey(p => p.IdClient)
-                .OnDelete(DeleteBehavior.Restrict);
-
-            modelBuilder.Entity<ProjectComment>()
-                .HasKey(p => p.Id);
-
-            modelBuilder.Entity<ProjectComment>()
-                .HasOne(p => p.Projects)
-                .WithMany(p => p.Comments)
-                .HasForeignKey(p => p.IdProject);
-
-            modelBuilder.Entity<ProjectComment>()
-           .HasOne(p => p.User)
-           .WithMany(p => p.Comments)
-           .HasForeignKey(p => p.IdUser);
-
-            modelBuilder.Entity<Skill>()
-                .HasKey(p => p.Id);
-
-            modelBuilder.Entity<User>()
-                .HasKey(p => p.Id);
-
-            modelBuilder.Entity<User>()
-                .HasMany(u => u.Skills)
-                .WithOne()
-                .HasForeignKey(u => u.IdSkill)
-                .OnDelete(DeleteBehavior.Restrict);
-
-            modelBuilder.Entity<UserSkill>()
-                .HasKey(p => p.Id);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());     
+                           
+                     
         }
     }
 
