187a9a6e91a709cfd3d425834539b8f633727a80
Jonathan Daniel Loth <ninoloth@gmail.com>
2022-05-26 19:40:41 -0300

    app Dapper

--- DevFreelas/DevFreelas.Application/Services/Implementations/SkillService.cs
+++ DevFreelas/DevFreelas.Application/Services/Implementations/SkillService.cs
@@ -1,6 +1,7 @@
 ﻿using DevFreelas.Application.Services.Interfaces;
 using DevFreelas.Application.ViewModels;
 using DevFreelas.Infrastructure.Persistence;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,10 +13,12 @@ namespace DevFreelas.Application.Services.Implementations
     public class SkillService : ISkillService
     {
         private readonly DevFreelasDbContext _dbContex;
+        private readonly string _connectionString;
 
-        public SkillService(DevFreelasDbContext dbContext)
+        public SkillService(DevFreelasDbContext dbContext, IConfiguration configuration)
         {
             _dbContex = dbContext;
+            _connectionString = configuration.GetConnectionString("DevFreelasCs");
         }
 
         public List<SkillViewModel> GetAll()
